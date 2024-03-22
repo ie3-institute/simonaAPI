f343733d6131e55be8e84cb69c6493eb8ea43797
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2024-03-22 16:10:36 +0100

    JavaDoc

--- src/main/java/edu/ie3/simona/api/data/ev/model/ArrivingEvsData.java
+++ src/main/java/edu/ie3/simona/api/data/ev/model/ArrivingEvsData.java
@@ -9,4 +9,11 @@ package edu.ie3.simona.api.data.ev.model;
 import java.util.List;
 import java.util.Optional;
 
-public record ArrivingEvsData(List<EvModel> arrivals, Optional<Long> nextTick) {}
+/**
+ * Data including arriving EVs and next arrival tick for a single charging station
+ *
+ * @param arrivals The arriving EVs for the current station
+ * @param maybeNextTick The optional next tick at which some interaction between EV simulation and
+ *     charging station is expected. Empty if no next event is known at this moment.
+ */
+public record ArrivingEvsData(List<EvModel> arrivals, Optional<Long> maybeNextTick) {}
