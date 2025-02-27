65d10b8736019682fc0239141b57c11976afbc47
staudtMarius <marius.staudt@tu-dortmund.de>
2025-02-27 14:07:12 +0100

    Some refactoring.

---
+++ src/main/java/edu/ie3/simona/api/data/results/model/DesaggFlexOptionsResult.java
@@ -0,0 +1,52 @@
+package edu.ie3.simona.api.data.results.model;
+
+import edu.ie3.datamodel.models.result.system.FlexOptionsResult;
+import tech.units.indriya.ComparableQuantity;
+
+import javax.measure.quantity.Power;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+public class DesaggFlexOptionsResult extends FlexOptionsResult {
+
+    private final Map<String, FlexOptionsResult> connectedFlexOptionResults;
+
+    public DesaggFlexOptionsResult(
+            ZonedDateTime time,
+            UUID inputModel,
+            ComparableQuantity<Power> pRef,
+            ComparableQuantity<Power> pMin,
+            ComparableQuantity<Power> pMax,
+            Map<String, FlexOptionsResult> connectedFlexOptionResults) {
+        super(time, inputModel, pRef, pMin, pMax);
+        this.connectedFlexOptionResults = connectedFlexOptionResults;
+    }
+
+    public DesaggFlexOptionsResult(
+            FlexOptionsResult flexOptionsResult,
+            Map<String, FlexOptionsResult> connectedFlexOptionResults) {
+        super(
+                flexOptionsResult.getTime(),
+                flexOptionsResult.getInputModel(),
+                flexOptionsResult.getpRef(),
+                flexOptionsResult.getpMin(),
+                flexOptionsResult.getpMax());
+        this.connectedFlexOptionResults = connectedFlexOptionResults;
+    }
+
+    public DesaggFlexOptionsResult(FlexOptionsResult flexOptionsResult) {
+        super(
+                flexOptionsResult.getTime(),
+                flexOptionsResult.getInputModel(),
+                flexOptionsResult.getpRef(),
+                flexOptionsResult.getpMin(),
+                flexOptionsResult.getpMax());
+        this.connectedFlexOptionResults = Collections.emptyMap();
+    }
+
+    public Map<String, FlexOptionsResult> getConnectedFlexOptionResults() {
+        return connectedFlexOptionResults;
+    }
+}
