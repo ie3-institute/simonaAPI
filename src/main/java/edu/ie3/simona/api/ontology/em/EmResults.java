7e3c93a9edfb87afe863d73341ce63a027b1ccc3
staudtMarius <marius.staudt@tu-dortmund.de>
2025-08-26 17:06:33 +0200

    Saving changes.

---
+++ src/main/java/edu/ie3/simona/api/ontology/em/EmResults.java
@@ -0,0 +1,9 @@
+package edu.ie3.simona.api.ontology.em;
+
+import edu.ie3.datamodel.models.result.ResultEntity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public record EmResults(Map<UUID, List<ResultEntity>> emResults) implements EmDataResponseMessageToExt {}
