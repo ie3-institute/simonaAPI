4e4f5f78f857fd2262fb3187f2e8a74ebb10e6c9
staudtMarius <marius.staudt@tu-dortmund.de>
2025-11-17 13:53:47 +0100

    Include reviewer's comments.

--- src/test/groovy/edu/ie3/simona/api/mapping/ExtEntityMappingTest.groovy
+++ src/test/groovy/edu/ie3/simona/api/mapping/ExtEntityMappingTest.groovy
@@ -16,6 +16,8 @@ import spock.lang.Shared
 import spock.lang.Specification
 import tech.units.indriya.quantity.Quantities
 
+import static edu.ie3.simona.api.mapping.DataType.*
+
 class ExtEntityMappingTest extends Specification {
     @Shared
     UUID loadUuid = UUID.fromString("22bea5fc-2cb2-4c61-beb9-b476e0107f52")
@@ -30,36 +32,17 @@ class ExtEntityMappingTest extends Specification {
     UUID emUuid = UUID.fromString("60dbc7e4-9718-4bbd-913a-dd26925e68a3")
 
     @Shared
-    ExtEntityEntry extResultEntry = new ExtEntityEntry(
-            loadUuid,
-            "Load",
-            Optional.empty(),
-            DataType.RESULT
-    )
+    ExtEntityEntry extResultEntry = new ExtEntityEntry(loadUuid, "Load", Optional.empty(), RESULT)
 
     @Shared
-    ExtEntityEntry extInputEntry = new ExtEntityEntry(
-            pvUuid,
-            "PV",
-            ColumnScheme.parse("p"),
-            DataType.PRIMARY
-    )
+    ExtEntityEntry extInputEntry = new ExtEntityEntry(pvUuid, "PV", ColumnScheme.parse("p"), PRIMARY)
 
     @Shared
-    ExtEntityEntry extPrimaryResultEntry = new ExtEntityEntry(
-            prUuid,
-            "PR",
-            ColumnScheme.parse("p"),
-            DataType.PRIMARY_RESULT
-    )
+    ExtEntityEntry extPrimaryResultEntry = new ExtEntityEntry(prUuid, "PR", ColumnScheme.parse("p"), PRIMARY_RESULT)
 
     @Shared
-    ExtEntityEntry extEmInputEntry = new ExtEntityEntry(
-            emUuid,
-            "Em",
-            Optional.empty(),
-            DataType.EM
-    )
+    ExtEntityEntry extEmInputEntry = new ExtEntityEntry(emUuid, "Em", Optional.empty(), EM)
+
 
     def "ExtEntityMapping can be created from a grid container correctly"() {
         given:
@@ -118,14 +101,13 @@ class ExtEntityMappingTest extends Specification {
         def mapping = new ExtEntityMapping(grid)
 
         when:
-        def updated = mapping.include(DataType.RESULT, ["ffi"], Optional.empty())
+        def updated = mapping.include(RESULT, ["ffi"], Optional.empty())
 
         then:
         // only the included ids will be returned, other grid ids will be ignored
-        mapping.getAssets(DataType.RESULT) == [node.uuid, participant.uuid, em.uuid]
-        updated.getAssets(DataType.RESULT) == [participant.uuid]
+        mapping.getAssets(RESULT) == [node.uuid, participant.uuid, em.uuid]
+        updated.getAssets(RESULT) == [participant.uuid]
     }
-
 
     def "ExtEntityMapping should return the data types correctly"() {
         when:
@@ -137,13 +119,13 @@ class ExtEntityMappingTest extends Specification {
 
         where:
         assets | expectedTypes
-        [extResultEntry] | [DataType.RESULT]
-        [extInputEntry] | [DataType.PRIMARY]
-        [extPrimaryResultEntry] | [DataType.PRIMARY_RESULT]
-        [extEmInputEntry] | [DataType.EM]
-        [extResultEntry, extInputEntry] | [DataType.RESULT, DataType.PRIMARY]
-        [extInputEntry, extEmInputEntry] | [DataType.PRIMARY, DataType.EM]
-        [extResultEntry, extInputEntry, extPrimaryResultEntry, extEmInputEntry] | [DataType.RESULT, DataType.PRIMARY, DataType.PRIMARY_RESULT, DataType.EM]
+        [extResultEntry] | [RESULT]
+        [extInputEntry] | [PRIMARY]
+        [extPrimaryResultEntry] | [PRIMARY_RESULT]
+        [extEmInputEntry] | [EM]
+        [extResultEntry, extInputEntry] | [RESULT, PRIMARY]
+        [extInputEntry, extEmInputEntry] | [PRIMARY, EM]
+        [extResultEntry, extInputEntry, extPrimaryResultEntry, extEmInputEntry] | [RESULT, PRIMARY, PRIMARY_RESULT, EM]
     }
 
     def "ExtEntityMapping should return all SIMONA uuid mapping correctly"() {
@@ -162,19 +144,6 @@ class ExtEntityMappingTest extends Specification {
         inputMap.get("Em") == emUuid
     }
 
-    def "ExtEntityMapping should return SIMONA uuid mapping correctly"() {
-        given:
-        def extAssetList = List.of(extResultEntry, extInputEntry, extPrimaryResultEntry, extEmInputEntry)
-        def extEntryMapping = new ExtEntityMapping(extAssetList)
-
-        when:
-        def actual = extEntryMapping.getAssets(DataType.PRIMARY)
-
-        then:
-        actual.size() == 1
-        actual.getFirst() == pvUuid
-    }
-
     def "ExtEntityMapping should return all external id mapping correctly"() {
         given:
         def extAssetList = List.of(extResultEntry, extInputEntry, extPrimaryResultEntry, extEmInputEntry)
@@ -189,5 +158,53 @@ class ExtEntityMappingTest extends Specification {
         inputMap.get(pvUuid) == "PV"
         inputMap.get(prUuid) == "PR"
         inputMap.get(emUuid) == "Em"
+    }
+
+    def "ExtEntityMapping should return SIMONA uuid mapping correctly"() {
+        given:
+        def extAssetList = [extResultEntry, extInputEntry, extPrimaryResultEntry, extEmInputEntry]
+        def extEntryMapping = new ExtEntityMapping(extAssetList)
+
+        when:
+        def actual = extEntryMapping.getAssets(PRIMARY)
+
+        then:
+        actual.size() == 1
+        actual.getFirst() == pvUuid
+    }
+
+    def "ExtEntityMapping should return assets uuids correctly"() {
+        given:
+        def node = new NodeInput(UUID.fromString("4476402e-d0e7-4975-935b-6861a00a2a5e"), "node", Quantities.getQuantity(1d, PowerSystemUnits.PU), false, NodeInput.DEFAULT_GEO_POSITION, GermanVoltageLevelUtils.LV, 1)
+        def em = new EmInput(UUID.fromString("d7c054b2-f5a8-4f88-99e1-59c734fc4655"), "em", "", null)
+        def participant = new FixedFeedInInput(UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2"), "ffi", node, null, em, Quantities.getQuantity(10, PowerSystemUnits.KILOVOLTAMPERE), 0.9)
+
+        List<AssetInput> gridAssets = [node]
+        List<SystemParticipantInput> participantInputs = [participant]
+
+        def grid = new SubGridContainer(
+                "test grid",
+                1,
+                new RawGridElements(gridAssets),
+                new SystemParticipants(participantInputs),
+                new GraphicElements([])
+        )
+
+        when:
+        def mapping = new ExtEntityMapping(grid).include(includedType, includedExtAssets, Optional.empty())
+        def actual = mapping.getAssets(requestedType)
+
+        then:
+        actual.size() == expectedSize
+        actual == expectedUuids
+
+        where:
+        includedType   | includedExtAssets  | requestedType  | expectedSize | expectedUuids
+        PRIMARY        | []                 | PRIMARY        | 1            | [UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2")]
+        PRIMARY_RESULT | ["ffi"]            | RESULT         | 1            | [UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2")]
+        RESULT         | []                 | RESULT         | 3            | [UUID.fromString("4476402e-d0e7-4975-935b-6861a00a2a5e"), UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2"), UUID.fromString("d7c054b2-f5a8-4f88-99e1-59c734fc4655")]
+        RESULT         | []                 | PRIMARY_RESULT | 1            | [UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2")]
+        RESULT         | ["ffi", "node"]    | RESULT         | 2            | [UUID.fromString("4476402e-d0e7-4975-935b-6861a00a2a5e"), UUID.fromString("4392e420-dc91-4867-a85f-b05d53896dd2")]
+
     }
 }
