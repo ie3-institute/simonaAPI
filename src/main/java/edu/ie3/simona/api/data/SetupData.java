ebe2d449b97fbb7f60627b63e6784977c0e1543f
staudtMarius <marius.staudt@tu-dortmund.de>
2025-11-28 11:38:46 +0100

    Fix sonarqube issue.

--- src/main/java/edu/ie3/simona/api/data/SetupData.java
+++ src/main/java/edu/ie3/simona/api/data/SetupData.java
@@ -9,6 +9,10 @@ package edu.ie3.simona.api.data;
 import com.typesafe.config.Config;
 import edu.ie3.datamodel.models.input.container.GridContainer;
 
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Container class that holds all data needed for setting up an external simulation.
  *
@@ -16,4 +20,32 @@ import edu.ie3.datamodel.models.input.container.GridContainer;
  * @param config The parsed simona config.
  * @param gridContainer The electrical grid.
  */
-public record SetupData(String[] mainArgs, Config config, GridContainer gridContainer) {}
+public record SetupData(String[] mainArgs, Config config, GridContainer gridContainer) {
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    SetupData setupData = (SetupData) o;
+    return Arrays.equals(mainArgs, setupData.mainArgs)
+        && Objects.equals(config, setupData.config)
+        && Objects.equals(gridContainer, setupData.gridContainer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(Arrays.hashCode(mainArgs), config, gridContainer);
+  }
+
+  @Override
+  @Nonnull
+  public String toString() {
+    return "SetupData{"
+        + "mainArgs="
+        + Arrays.toString(mainArgs)
+        + ", config="
+        + config
+        + ", gridContainer="
+        + gridContainer
+        + '}';
+  }
+}
