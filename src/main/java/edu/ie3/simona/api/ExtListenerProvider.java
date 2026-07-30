9cb5fa88f7d68eb1a6e2f61f3d59f45c57ae173b
staudtMarius <marius.staudt@tu-dortmund.de>
2026-07-30 17:18:59 +0200

    Saving changes.

--- src/main/java/edu/ie3/simona/api/ExtListenerProvider.java
+++ src/main/java/edu/ie3/simona/api/ExtListenerProvider.java
@@ -3,7 +3,7 @@ package edu.ie3.simona.api;
 import edu.ie3.simona.api.data.connection.ExtResultListener;
 import edu.ie3.simona.api.exceptions.ExtDataConnectionException;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * This interface needs to be implemented to provide SIMONA with external listeners.
@@ -14,7 +14,7 @@ public non-sealed interface ExtListenerProvider extends ExtLinkInterface {
      *
      * Returns the ext result listeners.
      */
-    default List<ExtResultListener> getResultListeners() {
+    default Set<ExtResultListener> getResultListeners() {
         throw new ExtDataConnectionException("No external result listeners provided!");
     }
 
